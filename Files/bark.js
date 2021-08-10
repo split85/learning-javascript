949baf8fdba9f05d2f2a1c3ea26f90b50ee33e13
split85 <hayat.omer@gmail.com>
2021-08-10 21:43:03 +0400

    1st function creation

---
+++ Files/bark.js
@@ -0,0 +1,12 @@
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF!!");
+      }  else {
+            console.log(name + " says woof woof!!");
+        }
+    }
+
+bark("rover", 23);
+bark("spot", 13);
+bark("spike", 53);
+bark("lady", 17);
